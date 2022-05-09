4fdf5f4483f9226aad37a08d1aaa77156419378c
Green Hornet <taphousecarl@gmail.com>
2022-05-09 10:15:05 -0400

    add migrations and create database

---
+++ Quail.api/Migrations/20220509140110_Migration2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quail.api.Migrations
+{
+    public partial class Migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_DepartmentId",
+                table: "Students",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_DepartmentId",
+                table: "Students");
+        }
+    }
+}
---
+++ Quail.api/Persistence/Entities/Department.cs
@@ -0,0 +1,7 @@
+namespace Quail.Api.Persistence.Entities;
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+}
